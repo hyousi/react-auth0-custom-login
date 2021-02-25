fd4d8bfc57b0eeb4c5142976b62531d3cd9e5e2f
zedang <ze.dang@thoughtworks.com>
2021-02-25 10:49:55 +0800

    Profile Page

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -8,6 +8,7 @@ interface AuthContextProps {
   authResult: any;
   login: (username: string, password: string) => Promise<any>;
   signup: (username: string, password: string) => Promise<any>;
+  logout: () => Promise<any>;
 }
 
 const initialAuthState: AuthContextProps = {
@@ -16,6 +17,7 @@ const initialAuthState: AuthContextProps = {
   authResult: null,
   login: () => new Promise((resolve) => resolve(null)),
   signup: () => new Promise((resolve) => resolve(null)),
+  logout: () => new Promise((resolve) => resolve(null)),
 };
 
 const AuthContext = createContext<AuthContextProps>(initialAuthState);
@@ -30,6 +32,7 @@ export function AuthProvider(props: React.PropsWithChildren<AuthOptions>) {
   const webAuth = new WebAuth(options);
   const history = useHistory();
   const [authResult, setAuthResult] = useState({});
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   /* add auth0 callback listener to parse & store auth0 response. */
   useEffect(() => {
@@ -41,6 +44,7 @@ export function AuthProvider(props: React.PropsWithChildren<AuthOptions>) {
         console.log('on parseHash');
         console.log(result);
         setAuthResult(result);
+        setIsAuthenticated(true);
         history.push('/');
       }
     });
@@ -71,10 +75,15 @@ export function AuthProvider(props: React.PropsWithChildren<AuthOptions>) {
     })
   }
 
+  function logout() {
+    return Promise.resolve("logout");
+  }
+
   return (
     <AuthContext.Provider
       value={{
         ...initialAuthState,
+        isAuthenticated,
         authResult,
         login,
         signup
